23107332add4a6dca941c0f137286c9bee15d121
rquispe <quispequispe.rm@gmail.com>
2016-03-23 12:43:37 +0000

    nuevos formularios

---
+++ softpark/app/controllers/trasaction_controller.rb
@@ -0,0 +1,19 @@
+class TrasactionController < ApplicationController
+  def list
+  end
+
+  def histori
+  end
+
+  def favorite
+  end
+
+  def detail
+  end
+
+  def paiment
+  end
+
+  def confirmation
+  end
+end
